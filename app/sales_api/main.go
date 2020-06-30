15487f8f395f3d168191cfdf6902b07b718ee5a8
Nokodoko <cMonty614@gmail.com>
2020-06-30 11:44:23 -0400

    serving

--- app/sales_api/main.go
+++ app/sales_api/main.go
@@ -3,18 +3,72 @@ package main
 import (
 	"log"
 	"os"
+	"time"
+
+	"github.com/ardanlabs/conf"
 )
 
 func main() {
 	//devs should only write to STDOUT
 	log := log.New(os.Stdout, "SALES : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 	if err := run(log); err != nil {
-		log.Prinln(err)
+		log.Println(err)
 		os.Exit(1)
 	}
 }
 
 func run(log *log.Logger) error {
 
+	// =========================================================================
+	// Configuration
+
+	var cfg struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:5s"`
+			ShutdownTimeout time.Duration `conf:"default:5s"`
+		}
+		DB struct {
+			User       string `conf:"default:postgres"`
+			Password   string `conf:"default:postgres,noprint"`
+			Host       string `conf:"default:db"`
+			Name       string `conf:"default:postgres"`
+			DisableTLS bool   `conf:"default:false"`
+		}
+		Auth struct {
+			KeyID          string `conf:"default:1"`
+			PrivateKeyFile string `conf:"default:/app/private.pem"`
+			Algorithm      string `conf:"default:RS256"`
+		}
+		Zipkin struct {
+			ReporterURI string  `conf:"default:http://zipkin:9411/api/v2/spans"`
+			ServiceName string  `conf:"default:sales-api"`
+			Probability float64 `conf:"default:0.05"`
+		}
+	//cfg.Version.SVN = build
+	cfg.Version.Desc = "copyright information here"
+
+	if err := conf.Parse(os.Args[1:], "SALES", &cfg); err != nil {
+		switch err {
+		case conf.ErrHelpWanted:
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		case conf.ErrVersionWanted:
+			version, err := conf.VersionString("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config version")
+			}
+			fmt.Println(version)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Nokodoko/project_1
 
 go 1.14
+
+require github.com/ardanlabs/conf v1.3.2
